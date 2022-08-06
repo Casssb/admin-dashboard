2339a3a676880f19ee520e695ed213f2d19a3633
Joe <caspar0912@googlemail.com>
2022-08-06 15:37:34 +0100

    added basic darkmode toggle

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 const menuToggle = document.querySelector('#nav-hamburger');
 const menuTitleText = document.querySelector('.nav-title-text');
 const menuIconText = document.querySelectorAll('.nav-text');
+const darkmodeToggle = document.querySelector('.darkmode-switch');
+const body = document.querySelector('body')
 
-function changeMenu () {
-    if (menuTitleText.style.display === 'none') {
-        menuTitleText.style.display = 'block';
-      } else {
-        menuTitleText.style.display = 'none';
-      }
-    
-      menuIconText.forEach((icon) => {
-        if (icon.style.display === 'none') {
-          icon.style.display = 'block';
-        } else {
-          icon.style.display = 'none';
-        }
-      });
-    
-      if (menuToggle.style.alignSelf === 'center') {
-        menuToggle.style.alignSelf = 'flex-end';
-      } else {
-        menuToggle.style.alignSelf = 'center'
-      }
+function changeMenu() {
+  if (menuTitleText.style.display === 'none') {
+    menuTitleText.style.display = 'block';
+  } else {
+    menuTitleText.style.display = 'none';
+  }
+
+  menuIconText.forEach((icon) => {
+    if (icon.style.display === 'none') {
+      icon.style.display = 'block';
+    } else {
+      icon.style.display = 'none';
+    }
+  });
+
+  if (menuToggle.style.alignSelf === 'center') {
+    menuToggle.style.alignSelf = 'flex-end';
+  } else {
+    menuToggle.style.alignSelf = 'center';
+  }
+}
+
+function changeTheme() {
+  body.classList.toggle('dark')
 }
 
 menuToggle.addEventListener('click', changeMenu);
+darkmodeToggle.addEventListener('click', changeTheme);
